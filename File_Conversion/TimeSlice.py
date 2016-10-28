b83b962ea101d9c8c2262f4c2f0d193ad6082fcd
Bryce <bxg167@case.edu>
2016-10-27 22:33:11 -0400

    Cleaned up the spacing and little things for RCFF and TimeSlice, and by me I mean my IDE.

--- File_Conversion/TimeSlice.py
+++ File_Conversion/TimeSlice.py
@@ -1,6 +1,5 @@
-class TimeSlice():
-
-	def __init__(self, pitch, volume, message):
-		self.pitch=pitch
-		self.volume = volume
-		self.message = message
+class TimeSlice:
+    def __init__(self, pitch, volume, message):
+        self.pitch = pitch
+        self.volume = volume
+        self.message = message
