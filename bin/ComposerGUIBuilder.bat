da95711ef6c2e7e90d30356b0f347578bc548d96
Bryce <bxg167@case.edu>
2016-12-03 20:50:04 -0500

    Added some GUI Builders, also changed the Queue we were using (Since the file name was the same as the object's name, the installer was having trouble using it.)

---
+++ bin/ComposerGUIBuilder.bat
@@ -0,0 +1,3 @@
+C:\python27\scripts\pyinstaller --clean --p "..\GUI" --p "..\File_Conversion" ..\GUI\ComposerGui.py
+.\dist\ComposerGui\ComposerGui.exe
+pause
---
+++ bin/ConverterGUIBuilder.bat
@@ -0,0 +1,3 @@
+C:\python27\scripts\pyinstaller --clean --p "..\GUI" --p "..\File_Conversion" ..\GUI\ConverterGui.py
+.\dist\ConverterGui\ConverterGui.exe
+pause
