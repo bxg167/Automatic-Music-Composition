4d54fd0b243919c0cb95ec305cc6899f602a9e35
Bryce <bxg167@case.edu>
2016-12-05 18:36:16 -0500

    - Made pyinstaller only create 1 executable (Instead of all of the other fluff) - Created an installer for the CMD - Converted CMD into Python 3.5

--- bin/ConverterGUIBuilder.bat
+++ bin/ConverterGUIBuilder.bat
@@ -1,3 +1,3 @@
-C:\python27\scripts\pyinstaller --clean --p "..\GUI" --p "..\File_Conversion" ..\GUI\ConverterGui.py
-.\dist\ConverterGui\ConverterGui.exe
+C:\python27\scripts\pyinstaller --clean --onefile --p "..\GUI" --p "..\File_Conversion" ..\GUI\ConverterGui.py
+.\dist\ConverterGui.exe
 pause
