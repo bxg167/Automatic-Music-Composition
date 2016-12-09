3f4d91067bd523b952afd130673c55ef30d34298
lrp39 <leah.platt@case.edu>
2016-12-09 13:56:21 -0500

    fixed error being thrown in tests, made it fail though

--- Tests/Unit_Tests/File_Conversion_Tests/test_converter_general.py
+++ Tests/Unit_Tests/File_Conversion_Tests/test_converter_general.py
@@ -1,8 +1,10 @@
+import os
 from unittest.case import TestCase
 
 from File_Conversion.Converter import Converter
 from File_Conversion.RCFF import RCFF
 
+file_path = os.path.dirname(__file__)
 
 class TestConverter(TestCase):
     def test_create_time_slices_from_note(self):
@@ -14,8 +16,8 @@ class TestConverter(TestCase):
         note = (time, length, pitch, volume)
 
         rcff = RCFF("C:\\Users\\Bryce", 10, "Sax")
-
-        rcff = Converter.__create_time_slices_from_note__(rcff, note)
+        c = Converter(os.path.abspath(os.path.join(file_path, "mary.mid")))
+        rcff = c.__create_time_slices_from_note__(rcff, note)
 
         self.assertEqual(length, len(rcff.body))
         for timeslice in rcff.body:
