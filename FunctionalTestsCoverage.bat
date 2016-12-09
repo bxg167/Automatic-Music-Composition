b563af5330e9bd66c15f205dc3e16fc40693b7da
Bryce <bxg167@case.edu>
2016-12-09 11:00:49 -0500

    Updated Testrunner to also print out errors

--- FunctionalTestsCoverage.bat
+++ FunctionalTestsCoverage.bat
@@ -1,5 +1,6 @@
-"C:\python27\python.exe" run_coverage.py erase
-"C:\python27\python.exe" run_coverage.py run -a .\Tests\testrunner.py
+py -2 setup.py install
+py -2 run_coverage.py erase
+py -2 run_coverage.py run ./Tests/testrunner.py -m --source="C:/Users/Bryce/PycharmProjects/Automatic-Music-Composition/Automatic-Music-Composition/File_Conversion/ConvertRcffToMidi.py"
 del "./htmlcov/" /Q
-"C:\python27\python.exe" run_coverage.py html
+py -2 run_coverage.py html
 pause
--- Tests/TestRunner.py
+++ Tests/TestRunner.py
@@ -19,6 +19,7 @@ main_suite.run(results)
 print "Results: "
 print "Number of Tests Ran: " + str(results.testsRun)
 print "Number of Failures: " + str(len(results.failures))
+print "Number of Errors: " + str(len(results.errors))
 
 if results.wasSuccessful():
     print "All tests passed." + "\n"
@@ -28,4 +29,10 @@ else:
     print "Failures: "
     for failure in results.failures:
         for info in failure:
+            print info
+
+    print
+    print "Errors: "
+    for errors in results.errors:
+        for info in errors:
             print info
